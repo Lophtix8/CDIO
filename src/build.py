0db8bd614979a1a13b86c455bb6557c05d581615
Adrian <kjellen.adrian99@gmail.com>
2024-10-08 14:38:28 +0200

    Started drafting a build()-function

--- src/build.py
+++ src/build.py
@@ -1,5 +1,20 @@
-def build(struct_data):
-    pass
+from ase.visualize import view
+from ase.io import read, write
+
+
+unit_cell = read("Ti2AlN.cif")
+
+TiAlN_super = mpstruct*(10,10,10)
+
+view(mpstruct)
+
+
+def build(config_file):
+    with open(config_file, 'r', newline="@") config_file:
+        materials = config_file.readlines()
+
+    for idx, line in enumerate(config_data):
+        materials.append(
 
 def delete_build():
     pass
