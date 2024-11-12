70e0c0d385c5736eb3130efa8a46477204a6e741
Lophtix8 <jonatan@wastlund.se>
2024-11-13 00:09:10 +0100

    Started with the code that creates directories and added logging to it.

--- fracture_md/job_queuer.py
+++ fracture_md/job_queuer.py
@@ -1,7 +1,39 @@
 import os
+import logging
+import copy
+import yaml
+from ase import formula
 
-def queue_job():
-	
+logger = logging.getLogger(__name__)
+
+def queue_job(config : list, poscar_filepath : str):
+    
+    crystal_name = get_crystal_name(poscar_filepath)
+    temps = config[temps]
+    
+    for temp in temps:
+        job_path = os.path.join(crystal_name, str(temp)+"K")
+
+        if not os.path.exists(job_path):
+            os.system(f"mkdir {job_path}")
+
+        poscar_name = os.path.basename(poscar_filepath)
+        dest_path = os.path.join(job_path, poscar_name)
+        
+        if os.path.exists(dest_path):
+            logger.warning("POSCAR already exists. Job skipped.")
+            raise FileExistsError
+
+        os.system(f"mv {poscar_filepath} {dest_path}")
+
+        temp_conf = copy.deepcopy(config)
+        temp_conf[temps] = temp
+
+        name = poscar_name.join(temp)
+        
+        with open(f'{name}.yaml', 'w') as file:
+            yaml.dump(temp_conf, file)
+
     template = get_template("template.q")
     job = write_job(template)    
 
@@ -51,5 +83,22 @@ def get_template(file_path: str) -> list[str]:
     
     return lines
 
-if __name__ == "__main__":
-	queue_job()
+def get_crystal_name(poscar_filepath : str):
+    crystal : str
+    try:
+        crystal = os.path.basename(poscar_filepath).removesuffix(".poscar")
+
+    except:
+        logger.error("Filepath needs to point to a poscar.")
+        raise NameError
+    
+    species_list : list[str]
+    for species in formula.species(crystal):
+        species_list.append(species)
+
+    species_list.sort()
+
+    return ''.join(species_list)
+
+if __name__ == "__main__": 
+    queue_job()
