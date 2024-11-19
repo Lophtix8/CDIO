03b1a04cc8a89786d4a032c7e130afb453c2782a
Adrian <kjellen.adrian99@gmail.com>
2024-11-19 17:41:10 +0100

    Unittests for build

--- Tests/Unittests/BuildTests.py
+++ Tests/Unittests/BuildTests.py
@@ -9,47 +9,20 @@ class BuildTests(ut.TestCase):
     def test_construct_supercell(self):
         NotInDB = build.construct_supercell("MadeUpMaterial",2,2,2)
         self.assertFalse(NotInDB)
-        
-        Al = build.construct_supercell("Al.poscar",2,2,2)
-        self.assertTrue(type(Al) == str)
-        self.assertTrue(os.path.isfile("Supercells/Al_2x2x2.poscar"))
-        self.assertFalse(os.path.isfile("Supercells/SiC_WrongDimensions.poscar"))
-        
-        SiC = build.construct_supercell("SiC.poscar",1,2,3)
-        self.assertTrue(type(SiC) == str)
-        self.assertTrue(os.path.isfile("Supercells/SiC_1x2x3.poscar"))
-        self.assertFalse(os.path.isfile("Supercells/SiC_WrongDimensions.poscar"))
 
     def test_construct_crack(self):
-        custom_fracture = build.construct_crack("Al.poscar", True, "x", "y", "z")
-        self.assertIsNone(custom_fracture)
-
-        build.construct_crack("SiC_1x2x3.poscar", False, [0,0.5], [0,0.5], [0,0.5])
-        self.assertTrue(os.path.isfile("Fractured_supercells/fractured_SiC_1x2x3.poscar"))
-        self.assertFalse(os.path.isfile("Fractured_supercells/SiC_WrongDimensions.poscar"))
-        with open("Fractured_supercells/fractured_SiC_1x2x3",'r') as f:
-            nr_atoms_frac = f.readlines()[6]
-        with open("Supercells/SiC_1x2x3",'r') as f:
-            nr_atoms = f.readlines()[6]
-        
-        self.assertGreaterEqual(int(nr_atoms),int(nr_atoms_frac))
+        custom_crack = build.construct_crack("AnyMaterial", True, 0, 0, 0)
+        self.assertFalse(custom_crack)
 
     def test_delete_build(self):
-        self.assertTrue(os.path.isfile("Fractured_supercells/fractured_SiC_1x2x3.poscar"))
-        build.delete_build("Fractured_supercells/fractured_SiC_1x2x3.poscar")
-        self.assertFalse(os.path.isfile("Fractured_supercells/fractured_SiC_1x2x3.poscar"))
-
-        self.assertTrue(os.path.isfile("Supercells/Al_2x2x2.poscar"))
-        build.delete_build("Supercells/Al_2x2x2.poscar")
-        self.assertFalse(os.path.isfile("Supercells/Al_2x2x2.poscar"))
-
-      
-    def test_main(self):
-        pass
+        f = open("FileToBeDeleted", 'w')
+        f.write("This will be deleted if the function works properly")
+        f.close()
+        self.assertTrue(os.path.isfile("FileToBeDeleted"))
+        build.delete_build("FileToBeDeleted")
+        self.assertFalse(os.path.isfile("FileToBeDeleted"))
 
 if __name__ == "__main__":
-    os.mkdir("Supercells")
-    os.mkdir("Fractured_supercells")
     tests = [ut.TestLoader().loadTestsFromTestCase(BuildTests)]
     testsuite = ut.TestSuite(tests)
     result = ut.TextTestRunner(verbosity=0).run(testsuite)
