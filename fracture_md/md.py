e1ad965871ad968e4978169d96a98833a384efda
Ludwig H <>
2024-11-19 20:24:12 +0100

    Initial try to relax fractured system before straining. seems to work okay but might need adjustments and more testing

--- fracture_md/md.py
+++ fracture_md/md.py
@@ -4,7 +4,8 @@ from ase.md.verlet import VelocityVerlet
 from ase import units
 from asap3 import Trajectory
 import numpy as np
-
+from asap3 import EMT
+from ase.calculators.lammpsrun import LAMMPS
 
 
 def calcenergy(a):
@@ -13,6 +14,7 @@ def calcenergy(a):
     int_T = ekin / (1.5 * units.kB)
     etot = epot + ekin
     return epot, ekin, int_T, etot
+    
 
 def run_md(supercell_file: str, temp: float, num_steps: int, strain_rate: int):
     """This function runs a molecular dynamics simulation and deposits the result in the 
@@ -27,22 +29,40 @@ def run_md(supercell_file: str, temp: float, num_steps: int, strain_rate: int):
     """
     
     from asap3 import EMT
-    file_path = 'Supercells/'
+    file_path = 'Fractured_supercells/'
         
     # Set up crystal
     crystal = read(file_path + supercell_file)
     
     # Describe the interatomic interactions with the Effective Medium Theory
-    crystal.calc = EMT()
+    # crystal.calc = EMT()
+    
+    # Pbc is by default set to true.
+    # crystal.pbc = [False, False, False]
+    parameters = {
+        'pair_style': 'tersoff',
+        'pair_coeff': ['* * SiC.tersoff Si C']
+    }
+    
+    files = ['SiC.tersoff']
+    
+    lammps = LAMMPS(files=files, **parameters)
+    
+    crystal.calc = lammps
     
     # Set the momenta corresponding to T=300K
     MaxwellBoltzmannDistribution(crystal, temperature_K=temp)
+    
+    # Relax the fractured system by running MD without strain in the beginning.
+    dyn_relax = VelocityVerlet(crystal, 5 * units.fs)
     
     # We want to run MD with constant energy using the VelocityVerlet algorithm.
     dyn = VelocityVerlet(crystal, 5 * units.fs)  # 5 fs time step.
     
     resultdata_file_name = "{file_name}.traj"
     traj = Trajectory('Simulation_results/' + resultdata_file_name.format(file_name = supercell_file.removesuffix('.poscar')), "w", crystal)
+    
+    dyn_relax.attach(traj.write, interval=10)
     dyn.attach(traj.write, interval=10)
 
     # Function to incrementally apply strain in the z-direction
@@ -62,10 +82,16 @@ def run_md(supercell_file: str, temp: float, num_steps: int, strain_rate: int):
               'Etot = %.3feV' % (epot, ekin, int_T, etot))
     
     # Run the dynamics
+    
+    # Relaxation
+    dyn_relax.attach(printenergy, interval=10)
+    dyn_relax.run(100)
+    
+    # Straining
     dyn.attach(printenergy, interval=10)
     printenergy()
     dyn.run(num_steps)
 
  
-#if __name__ == "__main__":
-#	run_md('Al_10x20x10.poscar',  300, 100, 0.01)
+if __name__ == "__main__":
+    run_md('fractured_Al_10x20x10.poscar', 300, 100, 0.01)
