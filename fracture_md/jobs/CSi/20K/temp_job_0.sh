7274a65958a285243fb7ccbeb8b0094e32716620
Adrian <kjellen.adrian99@gmail.com>
2024-11-25 17:02:41 +0100

    Start of making some more useful supercells

---
+++ fracture_md/jobs/CSi/20K/temp_job_0.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 /home/adriankjellen/CDIO/fracture_md/md.py /home/adriankjellen/CDIO/fracture_md/jobs/CSi/fractured_SiC_10x10x10.poscar jobs/CSi/20K/fractured_SiC_10x10x10_20K.yaml
